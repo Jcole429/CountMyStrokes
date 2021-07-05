649ae88676586336ce5cff4f4cfc8dd9142c50fb
Jcole429 <Justincole29@hotmail.com>
2021-07-05 16:00:07 -0400

    Mini golf GUI updates

--- CountMyStrokes/Views/MiniGolfModeView.swift
+++ CountMyStrokes/Views/MiniGolfModeView.swift
@@ -30,75 +30,64 @@ struct MiniGolfModeView: View {
             )
             .edgesIgnoringSafeArea(.all)
             VStack {
-                ZStack {
-                    Color(.darkGray)
-                        .cornerRadius(8)
-                        .opacity(0.5)
-                    VStack {
-                        Spacer()
-                        Text("Hole #\(model.miniGolfGameManager.game.currentHoleIndex + 1)")
-                            .font(.largeTitle)
-                        List {
-                            ForEach(model.miniGolfGameManager.game.players, id: \.playerId) { player in
-                                HoleInputSection(label: "\(player.name):  \(model.miniGolfGameManager.getPlayerStrokes(playerId: player.playerId))", button1ImageView: AnyView(Image(systemName: "minus")), button1Action: {
-                                    model.objectWillChange.send()
-                                    model.miniGolfGameManager.updatePlayerStrokes(playerId: player.playerId, incrementBy: -1)
-                                }, button2ImageView: AnyView(Image(systemName: "plus"))) {
-                                    model.objectWillChange.send()
-                                    model.miniGolfGameManager.updatePlayerStrokes(playerId: player.playerId, incrementBy: 1)
-                                }.listRowBackground(Color.clear)
-                            }
-                            .onDelete(perform: deleteItems)
-                            .buttonStyle(BorderlessButtonStyle())
-                        }.background(Color.clear)
-                    }
-                }
-                Button(action: {}, label: {
-                    Button1(label: "New Game") {
-                        newGamePressed = true
-                    }.alert(isPresented:$newGamePressed) {
-                        Alert(
-                            title: Text("New Game?"),
-                            message: Text("All data will be lost."),
-                            primaryButton: .destructive(Text("New Game")) {
-                                model.objectWillChange.send()
-                                model.miniGolfGameManager.newGame()
-                            },
-                            secondaryButton: .cancel()
-                        )
-                    }
-                })
                 Spacer()
-                HStack {
-
-                    Button1(label: "Hole #\(model.miniGolfGameManager.game.currentHoleIndex)") {
-                        model.objectWillChange.send()
-                        model.miniGolfGameManager.previousHole()
-                    }.opacity(model.miniGolfGameManager.game.currentHoleIndex > 0 ? 1 : 0)
+                VStack {
+                    Text("Hole #\(model.miniGolfGameManager.game.currentHoleIndex + 1)")
+                        .font(.largeTitle)
                     Spacer()
-                    Button1(label: "Hole #\(model.miniGolfGameManager.game.currentHoleIndex + 2)") {
-                        model.objectWillChange.send()
-                        model.miniGolfGameManager.nextHole()
-                    }.opacity(model.miniGolfGameManager.game.currentHoleIndex < 17 ? 1 : 0)
+                    ForEach(model.miniGolfGameManager.game.players, id: \.playerId) { player in
+                        HoleInputSection(label: "\(player.name):  \(model.miniGolfGameManager.getPlayerStrokes(playerId: player.playerId))", button1ImageView: AnyView(Image(systemName: "minus")), button1Action: {
+                            model.objectWillChange.send()
+                            model.miniGolfGameManager.updatePlayerStrokes(playerId: player.playerId, incrementBy: -1)
+                        }, button2ImageView: AnyView(Image(systemName: "plus"))) {
+                            model.objectWillChange.send()
+                            model.miniGolfGameManager.updatePlayerStrokes(playerId: player.playerId, incrementBy: 1)
+                        }.listRowBackground(Color.clear)
+                    }
+                    .onDelete(perform: deleteItems)
+                    .buttonStyle(BorderlessButtonStyle())
+                    Spacer()
                 }
+                .padding()
+                .background(Color(.darkGray)
+                                .opacity(0.7)
+                                .cornerRadius(8))
+                
+                Spacer()
+                VStack {
+                    Spacer()
+                    Button(action: {}, label: {
+                        Button1(label: "New Game") {
+                            newGamePressed = true
+                        }.alert(isPresented:$newGamePressed) {
+                            Alert(
+                                title: Text("New Game?"),
+                                message: Text("All data will be lost."),
+                                primaryButton: .destructive(Text("New Game")) {
+                                    model.objectWillChange.send()
+                                    model.miniGolfGameManager.newGame()
+                                },
+                                secondaryButton: .cancel()
+                            )
+                        }
+                    })
+                    Spacer()
+                    HStack {
+                        Button1(label: "Hole #\(model.miniGolfGameManager.game.currentHoleIndex)") {
+                            model.objectWillChange.send()
+                            model.miniGolfGameManager.previousHole()
+                        }.opacity(model.miniGolfGameManager.game.currentHoleIndex > 0 ? 1 : 0)
+                        Spacer()
+                        Button1(label: "Hole #\(model.miniGolfGameManager.game.currentHoleIndex + 2)") {
+                            model.objectWillChange.send()
+                            model.miniGolfGameManager.nextHole()
+                        }.opacity(model.miniGolfGameManager.game.currentHoleIndex < 17 ? 1 : 0)
+                    }
+                }
+                Spacer()
             }
-            .padding(.top, 50)
-            .padding(.horizontal, 30)
+            .padding()
             .foregroundColor(.white)
-            .navigationBarTitle("Mini Golf Mode")
-            .toolbar(content: {
-                NavigationLink(
-                    destination: AddPlayerView().environmentObject(model),
-                    label: {
-                        Text("Add Player")
-                            .font(.title2)
-                            .foregroundColor(.white)
-                            .padding(.horizontal, 20)
-                            .padding(.vertical, 10)
-                            .background(Color(UIColor.systemGreen))
-                            .cornerRadius(8)
-                    }).disabled(false).grayscale(0.8)
-            })
         }
     }
 }
