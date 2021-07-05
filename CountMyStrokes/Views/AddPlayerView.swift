1be31af4b12ad10f52ba52bd5127052911c2a10d
Jcole429 <Justincole29@hotmail.com>
2021-07-04 23:08:05 -0400

    Displaying mini golf players and allowing user to add or delete players

---
+++ CountMyStrokes/Views/AddPlayerView.swift
@@ -0,0 +1,36 @@
+//
+//  AddPlayerView.swift
+//  CountMyStrokes
+//
+//  Created by Justin Cole on 7/4/21.
+//
+
+import SwiftUI
+
+struct AddPlayerView: View {
+    
+    @EnvironmentObject var model: WatchConnectivityPhone
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    @State var newPlayerName = ""
+    @State private var action: Int? = 0
+    
+    var body: some View {
+        VStack{
+            Text("Add Player")
+            TextField("Player Name:", text: $newPlayerName)
+
+            Button1(label: "Add Player") {
+                model.objectWillChange.send()
+                model.miniGolfGameManager.addPlayer(playerName: newPlayerName)
+                self.presentationMode.wrappedValue.dismiss()
+            }
+        }
+    }
+}
+
+struct AddPlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddPlayerView()
+    }
+}
--- CountMyStrokes/Views/MiniGolfModeView.swift
+++ CountMyStrokes/Views/MiniGolfModeView.swift
@@ -11,17 +11,51 @@ struct MiniGolfModeView: View {
     
     @EnvironmentObject var model: WatchConnectivityPhone
     
+    func deleteItems(at offsets: IndexSet) {
+        model.objectWillChange.send()
+        model.miniGolfGameManager.game.players.remove(atOffsets: offsets)
+        model.miniGolfGameManager.updatePlayerNums()
+    }
     
     var body: some View {
-        NavigationView {
-            Text("Mini Golf Time")
-                .navigationBarTitle("Mini Golf Mode")
+        VStack {
+            List {
+                ForEach(model.miniGolfGameManager.game.players, id: \.playerId) { player in
+                        HoleInputSection(label: "\(player.name):  \(model.miniGolfGameManager.getPlayerStrokes(playerId: player.playerId))", button1ImageView: AnyView(Image(systemName: "minus")), button1Action: {
+                            model.objectWillChange.send()
+                            model.miniGolfGameManager.updatePlayerStrokes(playerId: player.playerId, incrementBy: -1)
+                        }, button2ImageView: AnyView(Image(systemName: "plus"))) {
+                            model.objectWillChange.send()
+                            model.miniGolfGameManager.updatePlayerStrokes(playerId: player.playerId, incrementBy: 1)
+                        }
+                }
+                .onDelete(perform: deleteItems)
+                .buttonStyle(BorderlessButtonStyle())
+            }
         }
+        .navigationBarTitle("Mini Golf Mode")
+        .toolbar(content: {
+            NavigationLink(
+                destination: AddPlayerView().environmentObject(model),
+                label: {
+                    Text("Add Player")
+                        .font(.title2)
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 10)
+                        .background(Color(UIColor.systemGreen))
+                        .cornerRadius(8)
+                }).disabled(false).grayscale(0.8)
+        })
     }
 }
 
 struct MiniGolfMode_Previews: PreviewProvider {
+    
     static var previews: some View {
-        MiniGolfModeView()
+        let testModel = WatchConnectivityPhone()
+        testModel.miniGolfGameManager.addPlayer(playerName: "Justin")
+        testModel.miniGolfGameManager.addPlayer(playerName: "Bohdan")
+        return MiniGolfModeView().environmentObject(testModel)
     }
 }
