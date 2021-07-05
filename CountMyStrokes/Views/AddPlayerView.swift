a009e7c86c5cdcb363b20eabfe8ff21210aae882
Jcole429 <Justincole29@hotmail.com>
2021-07-05 16:26:06 -0400

    Updating AddPlayerView

--- CountMyStrokes/Views/AddPlayerView.swift
+++ CountMyStrokes/Views/AddPlayerView.swift
@@ -16,15 +16,40 @@ struct AddPlayerView: View {
     @State private var action: Int? = 0
     
     var body: some View {
-        VStack{
-            TextField("Player Name:", text: $newPlayerName)
-
-            Button1(label: "Add Player") {
-                self.model.objectWillChange.send()
-                model.miniGolfGameManager.addPlayer(playerName: newPlayerName)
-                self.presentationMode.wrappedValue.dismiss()
+        ZStack {
+            Color.clear.overlay(
+                Image("golf-background")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+            ).edgesIgnoringSafeArea(.all)
+            VStack {
+                Spacer()
+                VStack {
+                    TextField("Player Name:", text: $newPlayerName)
+                        .padding()
+                }
+                .background(Color(.darkGray)
+                                .opacity(0.7)
+                                .cornerRadius(8))
+                .padding()
+                Button1(label: "Add Player") {
+                    if ($newPlayerName.wrappedValue != "") {
+                        self.model.objectWillChange.send()
+                        model.miniGolfGameManager.addPlayer(playerName: newPlayerName)
+                        self.presentationMode.wrappedValue.dismiss()
+                    }
+                }
+                Spacer()
+                HStack {
+                    Spacer()
+                    Text("Golf Vectors by Vecteezy")
+                        .padding(.trailing)
+                        .foregroundColor(.white)
+                    
+                }
             }
         }
+        .foregroundColor(.white)
     }
 }
 
